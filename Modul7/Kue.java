bf7fc0fdd1435200975262ebc3f4f62b7915a82d
Andrian Indyanto <indyanto07@gmail.com>
2026-04-13 23:15:31 +0700

    Add files via upload

--- Modul7/Kue.java
+++ Modul7/Kue.java
@@ -1,1 +1,35 @@
+package tugaspraktikum.tugaspraktikum7;
 
+abstract class Kue {
+    private String nama;
+    private Double harga;
+
+    public Kue(String nama, Double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public Double getHarga() {
+        return harga;       
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setHarga(Double harga) {
+        this.harga = harga;
+    }
+
+    public double hitungHarga(){
+        return harga;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nama Kue: %-20s \n| Harga Dasar: Rp%,.2f", nama, harga);
+    }
+    
+}
