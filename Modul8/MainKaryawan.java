dfb661987795fdc676685e115f307fbcd0e9d425
Andrian Indyanto <indyanto07@gmail.com>
2026-04-21 17:12:38 +0700

    Add files via upload

--- Modul8/MainKaryawan.java
+++ Modul8/MainKaryawan.java
@@ -3,14 +3,14 @@ package tugaspraktikum.tugaspraktikum8;
 public class MainKaryawan {
     public static void main(String[] args) {
 
-        Invoice belanja1 = new Invoice("Susu Beruang", 2, 15000);
-        Invoice belanja2 = new Invoice("Beras 5kg", 1, 75000);
+        Invoice belanja1 = new Invoice("Shampo", 2, 15000);
+        Invoice belanja2 = new Invoice("Pasta Gigi", 1, 75000);
         Invoice belanja3 = new Invoice("Sabun Mandi", 3, 5000);
        
         Invoice[] listBelanja = {belanja1, belanja2, belanja3};
 
 
-        Employee emp = new Employee(1024, "Budi Meneer", 5000000, listBelanja);
+        Employee emp = new Employee(1234567890, "Pak bahlil", 5000000, listBelanja);
 
         emp.displayInfo();
     }
