0745429cf3c67080e09e9758fd82ae6f9a4a936f
Andrian Indyanto <indyanto07@gmail.com>
2026-05-01 18:08:06 +0700

    Add files via upload

--- Modul 9/bab 8/asd.java
+++ Modul 9/bab 8/asd.java
@@ -1,1 +1,9 @@
+package tugaspraktikum.tugaspraktikum9;
 
+public class asd extends MataKuliah {
+    @Override
+    public double hitungNilai(double tugas, double kuis, double uts, double uas) {
+        // Rumus ASD: Tugas(20%), Kuis(20%), UTS(30%), UAS(30%)
+        return (tugas * 0.20) + (kuis * 0.20) + (uts * 0.30) + (uas * 0.30);
+    }
+}
