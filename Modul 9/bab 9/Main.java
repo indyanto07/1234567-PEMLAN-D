f13cd621a398942966a6b9e7a0b2409fdce3e6ca
Andrian Indyanto <indyanto07@gmail.com>
2026-05-01 20:26:17 +0700

    Add files via upload

---
+++ Modul 9/bab 9/Main.java
@@ -0,0 +1,24 @@
+package tugaspraktikum.tugaspraktikum9.bab9;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+      
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                
+           
+                FormDaftarUlang formUtama = new FormDaftarUlang();
+                formUtama.setVisible(true);
+            }
+        });
+    }
+}
