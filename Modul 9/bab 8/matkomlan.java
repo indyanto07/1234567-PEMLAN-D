195100228fb2460afcfd28536928a7d91e1c1613
Andrian Indyanto <indyanto07@gmail.com>
2026-05-01 18:16:40 +0700

    Update matkomlan.java

--- Modul 9/bab 8/matkomlan.java
+++ Modul 9/bab 8/matkomlan.java
@@ -4,6 +4,6 @@ public class matkomlan extends MataKuliah {
     @Override
     public double hitungNilai(double tugas, double kuis, double uts, double uas) {
         // Rumus Matkomlan: Tugas(10%), Kuis(20%), UTS(30%), UAS(40%)
-        return (tugas / 0.10) + (kuis * 0.20) + (uts * 0.30) + (uas * 0.40);
+        return (tugas * 0.10) + (kuis * 0.20) + (uts * 0.30) + (uas * 0.40);
     }
 }
