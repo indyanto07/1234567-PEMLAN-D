2661605bb14cfbd29df82b5d745c32e5bfd9b325
Andrian Indyanto <indyanto07@gmail.com>
2026-05-08 20:05:53 +0700

    Add files via upload

--- Modul10/cekMemoriFile.java
+++ Modul10/cekMemoriFile.java
@@ -1,1 +1,18 @@
+package tugaspraktikum.tugaspraktikum10;
+import java.io.File;
+public class cekMemoriFile {
+    public static void main(String[] args) {
+        var file = new File("test.txt");
+        if (file.exists()) {
+            long memori = file.length();
 
+            if (memori < 1024) {
+                System.out.println("Ukuran file: " + memori + " KB");
+            } else if (memori < 1024 * 1024) {
+                System.out.println("Ukuran file: " + memori / 1024.0 + " MB");
+            } 
+        } else {
+            System.out.println("File tidak ada");
+        }
+    }
+}
