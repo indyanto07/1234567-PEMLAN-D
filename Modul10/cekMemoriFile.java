9e7940db99f43cd14b02afa5a83bc2c4399ce714
Andrian Indyanto <indyanto07@gmail.com>
2026-05-08 20:13:32 +0700

    Add files via upload

--- Modul10/cekMemoriFile.java
+++ Modul10/cekMemoriFile.java
@@ -6,11 +6,11 @@ public class cekMemoriFile {
         if (file.exists()) {
             long memori = file.length();
 
-            if (memori < 1024) {
-                System.out.println("Ukuran file: " + memori + " KB");
-            } else if (memori < 1024 * 1024) {
-                System.out.println("Ukuran file: " + memori / 1024.0 + " MB");
-            } 
+            if  (memori < 1024 * 1024) {
+                System.out.printf("Ukuran file: %.2f KB%n", memori / 1024.0);
+            } else {
+                System.out.printf("Ukuran file: %.2f MB%n", memori / (1024.0 * 1024.0));
+            }
         } else {
             System.out.println("File tidak ada");
         }
