b4934dd7f20b993f64e48936bce6b08c34829408
Andrian Indyanto <indyanto07@gmail.com>
2026-05-15 17:36:51 +0700

    Add files via upload

--- Modul 11/urutTinggiBadan.java
+++ Modul 11/urutTinggiBadan.java
@@ -1,5 +1,10 @@
 package tugaspraktikum.tugaspraktikum11;
 
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class urutTinggiBadan {
     public static void main(String[] args) {
         int[] TbA = {168, 170, 165, 168, 172, 170, 169, 165, 171, 166};
@@ -13,15 +18,10 @@ public class urutTinggiBadan {
         for (int i = 0; i < TbB.length; i++) {
             gabungan[TbA.length + i] = TbB[i];
         }
-        for (int i = 0; i < gabungan.length - 1; i++) {
-            for (int j = 0; j < gabungan.length - i - 1; j++) {
-                if (gabungan[j] > gabungan[j + 1]) {
-                    int temp = gabungan[j];
-                    gabungan[j] = gabungan[j + 1];
-                    gabungan[j + 1] = temp;
-                }
-            }
-        }
+        
+        List<Integer> list = Arrays.stream(gabungan).boxed().collect(java.util.stream.Collectors.toList());
+
+
         System.out.println("Tinggi badan team A:");
         for (int data : TbA) {
             System.out.print(data + " ");
@@ -36,24 +36,10 @@ public class urutTinggiBadan {
         System.out.println();
         System.out.println();
         
-        System.out.println("Tinggi badan yang diurut dengan Ascending:");
-        for (int tinggi : gabungan) {
-            System.out.print(tinggi + " ");
-        }
-        
-        System.out.println();
-        for (int i = 0; i < gabungan.length - 1; i++) {
-            for (int j = 0; j < gabungan.length - i - 1; j++) {
-                if (gabungan[j] < gabungan[j + 1]) {
-                    int temp = gabungan[j];
-                    gabungan[j] = gabungan[j + 1];
-                    gabungan[j + 1] = temp;
-                }
-            }
-        }
-        System.out.println("\nTinggi badan yang diurut dengan Descending:");
-        for (int tinggi : gabungan) {
-            System.out.print(tinggi + " ");
-        }
+        Collections.sort(list);
+        System.out.println("Tinggi badan yang diurut dengan Ascending: "+list);
+       
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println("\nTinggi badan yang diurut dengan Descending: "+list);
     }
 }
